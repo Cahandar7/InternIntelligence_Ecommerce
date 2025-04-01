c6bee7f80d13e9a492c320972a18e54b8aaa1f94
Cahandar7 <cahandar717@gmail.com>
2025-04-01 23:35:55 +0400

    image magnify prod details translate

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -7,7 +7,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShirt, faStar } from "@fortawesome/free-solid-svg-icons";
 import { Container, Row, Col, Dropdown } from "react-bootstrap";
 import size_guide_img from "../assets/images/size_guide.png";
-// import ReactImageMagnify from "react-image-magnify";
 import slugify from "slugify";
 import { useWishlist } from "../contexts/WishlistContext";
 import { LanguageContext } from "../contexts/LanguageContext";
@@ -18,6 +17,27 @@ const ProductDetails = () => {
   const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
   const { language } = useContext(LanguageContext);
+  const [isZoomed, setIsZoomed] = useState(false);
+  const [position, setPosition] = useState({ top: 0, left: 0 });
+
+  const handleMouseEnter = () => {
+    setIsZoomed(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsZoomed(false);
+  };
+
+  const handleMouseMove = (e) => {
+    const imageContainer = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - imageContainer.left;
+    const y = e.clientY - imageContainer.top;
+
+    setPosition({
+      top: (y / imageContainer.height) * 100,
+      left: (x / imageContainer.width) * 100,
+    });
+  };
 
   const brand_logos = {
     escada:
@@ -94,41 +114,37 @@ const ProductDetails = () => {
     </div>
   ) : (
     <div className="page">
-      <AnimatedTopHero page={"shop"} second_page={selectedProd.category} />
+      <AnimatedTopHero
+        page={
+          language === "en" ? "Shop" : language === "ru" ? "Магазин" : "Mağaza"
+        }
+        second_page={selectedProd.category}
+      />
       <Container className="product-details-container">
         <Row>
-          <Col sm={12} md={6} lg={5}>
-            <img src={selectedProd.image} alt={selectedProd.title} />
-            {/* <ReactImageMagnify
-              {...{
-                smallImage: {
-                  alt: selectedProd.title,
-                  isFluidWidth: true,
-                  src: selectedProd.image,
-                },
-                largeImage: {
-                  src: selectedProd.image,
-                  width: 1200,
-                  height: 1800,
-                },
-                lensStyle: {
-                  backgroundColor: "transparent",
-                  border: "none",
-                },
-                enlargedImageContainerDimensions: {
-                  width: "100%",
-                  height: "100%",
-                },
-                enlargedImageContainerStyle: {
-                  zIndex: 10,
-                  position: "absolute",
-                  top: "0",
-                  left: "0",
-                  border: "none",
-                },
-                zoomPosition: "center",
-              }}
-            /> */}
+          <Col
+            sm={12}
+            md={6}
+            lg={5}
+            className="image-container"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onMouseMove={handleMouseMove}
+          >
+            <img
+              src={selectedProd.image}
+              alt={selectedProd.title}
+              className="product-image"
+            />
+            {isZoomed && (
+              <div
+                className="magnified-image"
+                style={{
+                  backgroundImage: `url(${selectedProd.image})`,
+                  backgroundPosition: `${position.left}% ${position.top}%`,
+                }}
+              ></div>
+            )}
           </Col>
           <Col sm={12} md={6} lg={4} className="product-details-texts">
             <p className="product-details-title">{selectedProd.title}</p>
@@ -169,7 +185,13 @@ const ProductDetails = () => {
                 size="lg"
                 style={{ color: "#888888" }}
               />
-              <span style={{ color: "#888888" }}>Sizing guide</span>
+              <span style={{ color: "#888888" }}>
+                {language === "en"
+                  ? "Sizing guide"
+                  : language === "ru"
+                  ? "Руководство по размеру"
+                  : "Ölçü bələdçisi"}
+              </span>
               {isOpen ? (
                 <>
                   <div
@@ -199,21 +221,47 @@ const ProductDetails = () => {
               ) : null}
             </div>
             <div className="other-info">
-              Weight<span>{selectedProd.weight}</span>
+              {language === "en"
+                ? "Weight"
+                : language === "ru"
+                ? "Вес"
+                : "Çəki"}
+              <span>{selectedProd.weight}</span>
             </div>
             <div className="other-info">
-              Dimensions<span>{selectedProd.dimensions}</span>
+              {language === "en"
+                ? "Dimensions"
+                : language === "ru"
+                ? "Размеры"
+                : "Ölçülər"}
+              <span>{selectedProd.dimensions}</span>
             </div>
             <div className="other-info">
-              Washcare<span>{selectedProd.washcare}</span>
+              {language === "en"
+                ? "Washcare"
+                : language === "ru"
+                ? "Стирка"
+                : "Yuma təlimatları"}
+              <span>{selectedProd.washcare}</span>
             </div>
             <div className="other-info">
-              Composition
+              {language === "en"
+                ? "Composition"
+                : language === "ru"
+                ? "Состав"
+                : "Tərkibi"}
               <span>{selectedProd.composition}</span>
             </div>
+
             <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
               <div className="product-details-dropdown">
-                <p>Color</p>
+                <p>
+                  {language === "en"
+                    ? "Color"
+                    : language === "ru"
+                    ? "Цвет"
+                    : "Rəng"}
+                </p>
                 <Dropdown onSelect={setActiveColor}>
                   <Dropdown.Toggle variant="dark" id="dropdown-basic">
                     {activeColor}
@@ -224,31 +272,53 @@ const ProductDetails = () => {
                       eventKey="Red"
                       className={activeColor === "Red" ? "active-item" : ""}
                     >
-                      Red
+                      {language === "en"
+                        ? "Red"
+                        : language === "ru"
+                        ? "Красный"
+                        : "Qırmızı"}
                     </Dropdown.Item>
                     <Dropdown.Item
                       eventKey="Blue"
                       className={activeColor === "Blue" ? "active-item" : ""}
                     >
-                      Blue
+                      {language === "en"
+                        ? "Blue"
+                        : language === "ru"
+                        ? "Синий"
+                        : "Mavi"}
                     </Dropdown.Item>
                     <Dropdown.Item
                       eventKey="Green"
                       className={activeColor === "Green" ? "active-item" : ""}
                     >
-                      Green
+                      {language === "en"
+                        ? "Green"
+                        : language === "ru"
+                        ? "Зеленый"
+                        : "Yaşıl"}
                     </Dropdown.Item>
                     <Dropdown.Item
                       eventKey="Black"
                       className={activeColor === "Black" ? "active-item" : ""}
                     >
-                      Black
+                      {language === "en"
+                        ? "Black"
+                        : language === "ru"
+                        ? "Черный"
+                        : "Qara"}
                     </Dropdown.Item>
                   </Dropdown.Menu>
                 </Dropdown>
               </div>
               <div className="product-details-dropdown">
-                <p>Size</p>
+                <p>
+                  {language === "en"
+                    ? "Size"
+                    : language === "ru"
+                    ? "Размер"
+                    : "Ölçü"}
+                </p>
                 <Dropdown onSelect={setActiveSize}>
                   <Dropdown.Toggle variant="dark" id="dropdown-basic">
                     {activeSize}
@@ -283,6 +353,7 @@ const ProductDetails = () => {
                 </Dropdown>
               </div>
             </div>
+
             <div style={{ display: "flex", alignItems: "center", gap: "15px" }}>
               <span
                 className="product-details-btn"
@@ -299,7 +370,11 @@ const ProductDetails = () => {
                 >
                   <path d="M23.76 4.248c-0.096-0.096-0.24-0.24-0.504-0.24h-18.48l-0.48-2.4c-0.024-0.288-0.384-0.528-0.624-0.528h-2.952c-0.384 0-0.624 0.264-0.624 0.624s0.264 0.648 0.624 0.648h2.424l2.328 11.832c0.312 1.608 1.848 2.856 3.48 2.856h11.28c0.384 0 0.624-0.264 0.624-0.624s-0.264-0.624-0.624-0.624h-11.16c-0.696 0-1.344-0.312-1.704-0.816l14.064-1.92c0.264 0 0.528-0.24 0.528-0.528l1.968-7.824v-0.024c-0.024-0.048-0.024-0.288-0.168-0.432zM22.392 5.184l-1.608 6.696-14.064 1.824-1.704-8.52h17.376zM8.568 17.736c-1.464 0-2.592 1.128-2.592 2.592s1.128 2.592 2.592 2.592c1.464 0 2.592-1.128 2.592-2.592s-1.128-2.592-2.592-2.592zM9.888 20.328c0 0.696-0.624 1.32-1.32 1.32s-1.32-0.624-1.32-1.32 0.624-1.32 1.32-1.32 1.32 0.624 1.32 1.32zM18.36 17.736c-1.464 0-2.592 1.128-2.592 2.592s1.128 2.592 2.592 2.592c1.464 0 2.592-1.128 2.592-2.592s-1.128-2.592-2.592-2.592zM19.704 20.328c0 0.696-0.624 1.32-1.32 1.32s-1.344-0.6-1.344-1.32 0.624-1.32 1.32-1.32 1.344 0.624 1.344 1.32z"></path>
                 </svg>
-                Add to Cart
+                {language === "en"
+                  ? "Add to Cart"
+                  : language === "ru"
+                  ? "В корзину"
+                  : "Səbətə at"}
               </span>
               <span
                 className="product-details-btn"
@@ -323,16 +398,18 @@ const ProductDetails = () => {
                 >
                   <path
                     d="M99.5,31.5C98.4,17.2,86.3,5.7,71.9,5.3C63.8,5,55.6,8.5,50,14.5C44.3,8.4,36.4,5,28.1,5.3C13.7,5.7,1.6,17.2,0.5,31.5
-                    c-0.1,1.2-0.1,2.5-0.1,3.7c0.2,5.1,2.4,10.2,6.1,14.3l39.2,43.4c1.1,1.2,2.7,1.9,4.3,1.9c1.6,0,3.2-0.7,4.4-1.9l39.1-43.4
-                    c3.7-4.1,5.8-9.1,6.1-14.3C99.6,34,99.6,32.7,99.5,31.5z M49.6,89.2L10.5,45.8c-2.8-3.1-4.5-7-4.7-10.9c0-1,0-2,0.1-3
-                    C6.8,20.4,16.6,11,28.2,10.7c0.2,0,0.5,0,0.7,0c7.4,0,14.5,3.6,18.8,9.7c0.5,0.7,1.3,1.1,2.2,1.1s1.7-0.4,2.2-1.1
-                    c4.5-6.3,11.8-9.9,19.6-9.7c11.6,0.4,21.4,9.7,22.4,21.2c0.1,1,0.1,2,0.1,3v0c-0.2,3.9-1.8,7.8-4.7,10.9L50.4,89.2
-                    C50.2,89.4,49.7,89.3,49.6,89.2z"
-                  ></path>
+        c-0.1,1.2-0.1,2.5-0.1,3.7c0.2,5.1,2.4,10.2,6.1,14.3l39.2,43.4c1.1,1.2,2.7,1.9,4.3,1.9c1.6,0,3.2-0.7,4.4-1.9l39.1-43.4
+        c3.7-4.1,5.8-9.1,6.1-14.3C99.6,34,99.6,32.7,99.5,31.5z M49.6,89.2L10.5,45.8c-2.8-3.2-4.2-7.2-4.4-11.4c-0.1-2.2,0.4-4.4,1.4-6.3
+        c1.1-1.9,2.7-3.4,4.7-4.2c2.3-0.9,4.7-1.2,7.2-1.1c5.2,0.1,9.8,2.4,13.1,6.2c1.3,1.4,2.5,3,3.4,4.7c0.9-1.7,2.1-3.3,3.4-4.7
+        c3.3-3.8,7.8-6.1,13-6.2c2.4-0.1,4.8,0.2,7.1,1.1c2.1,0.8,3.7,2.3,4.8,4.2c1,1.9,1.5,4.1,1.4,6.3c-0.2,4.2-1.6,8.3-4.4,11.4
+        L49.6,89.2z"
+                  />
                 </svg>
-                {isInWishlist(selectedProd)
-                  ? "Remove from Wishlist"
-                  : "Add to Wishlist"}
+                {language === "en"
+                  ? "Add to Wishlist"
+                  : language === "ru"
+                  ? "В избранное"
+                  : "Bəyənilənlərə at"}
               </span>
               <span
                 className="product-details-btn"
@@ -340,7 +417,11 @@ const ProductDetails = () => {
                   history.back();
                 }}
               >
-                Back
+                {language === "en"
+                  ? "Back"
+                  : language === "ru"
+                  ? "Назад"
+                  : "Geri"}
               </span>
             </div>
           </Col>
@@ -352,10 +433,20 @@ const ProductDetails = () => {
                 alt={selectedProd.brand}
               />
               <Link to={"/shop"} className="link">
-                View all products
+                {language === "en"
+                  ? "View all products"
+                  : language === "ru"
+                  ? "Посмотреть все товары"
+                  : "Bütün məhsulları gör"}
               </Link>
             </div>
-            <div className="you-also-like">You may also like...</div>
+            <div className="you-also-like">
+              {language === "en"
+                ? "You may also like..."
+                : language === "ru"
+                ? "Вам также может понравиться..."
+                : "Sizə də bəyənə bilərsiniz..."}
+            </div>
             <Row>
               {products
                 .filter(
